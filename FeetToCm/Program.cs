ca2b1ec6053db6973a4b753dfea946ee341e1090
harshkr-cs <harshwwe555@gmail.com>
2026-01-30 09:43:30 +0530

    FeetToCm

---
+++ FeetToCm/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main()
+    {
+
+        double output = helper();
+        Console.WriteLine($"Output is: {output}");
+
+    }
+
+    public static double helper()
+    {
+        int Feet = int.Parse(Console.ReadLine());
+        if (Feet < 0 || Feet > int.MaxValue)
+        {
+            Console.WriteLine("Invalid Feet Value");
+            return 0;
+        }
+
+        double res = Math.Round(30.48 * Feet, MidpointRounding.AwayFromZero);
+        return res;
+
+    }
+}
