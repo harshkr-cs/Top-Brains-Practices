cb5e8cdd706cff3a48e34001c53d88fc56d75d6e
harshkr-cs <harshwwe555@gmail.com>
2026-01-30 09:32:32 +0530

    ModPointRoundToZero

---
+++ MidPointRound/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        double output = CalculateCircleArea();
+        Console.WriteLine(output);
+    }
+
+    public static double CalculateCircleArea()
+    {
+        double radius = double.Parse(Console.ReadLine());
+
+        if (radius < 0 || radius > 1e6)
+        {
+            Console.WriteLine("Invalid radius");
+            return 0;
+        }
+
+        double area = Math.PI * radius * radius;
+        double rounded = Math.Round(area, 2, MidpointRounding.AwayFromZero);
+        return rounded;
+    }
+}
