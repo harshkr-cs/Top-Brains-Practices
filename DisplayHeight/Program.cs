6845bbc7ef599aea319471b0331248f69785b83d
harshkr-cs <harshwwe555@gmail.com>
2026-01-30 09:55:05 +0530

    DisplayHeight

---
+++ DisplayHeight/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System ;
+
+public class DisplayHeight
+{
+    public static void Main (string[] args)
+    {
+        Console.Write("Enter The Height in Centimeters (Integer) : ") ;
+        int height = Convert.ToInt32(Console.ReadLine()) ;
+
+        if (height <= 0 || height >= 300)
+        {
+            Console.WriteLine("Invalid Input") ;
+            return ;
+        }
+
+        if (height >= 180)
+        {
+            Console.WriteLine("Tall") ;
+        }
+        else if (height >= 150)
+        {
+            Console.WriteLine("Average") ;
+        }
+        else
+        {
+            Console.WriteLine("Short") ;
+        }
+    }
+}
