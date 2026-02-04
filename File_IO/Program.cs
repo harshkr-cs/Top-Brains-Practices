79bf2f39a3b171d30f6e2c4d426d1c687cad7569
harshkr-cs <harshwwe555@gmail.com>
2026-02-04 12:06:41 +0530

    File_IO

---
+++ File_IO/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        string inputFile="Log.txt";
+        string outputFile="error.txt";
+        try
+        {
+            string[] lines = File.ReadAllLines(inputFile);
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (string line in lines)
+                {
+                    if (line.Contains("ERROR"))
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            Console.WriteLine("ERROR logs extracted successfully into error.txt");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Mylog.txt file not found!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
