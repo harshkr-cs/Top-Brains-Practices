9c89cbad3eab66aa3b614fffbfea58d346637a3a
harshkr-cs <harshwwe555@gmail.com>
2026-02-04 11:21:46 +0530

    BankTransaction

---
+++ BankTransaction/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    public static int BankTransaction(int initialBalance,int[] transactions)
+    {
+        int balance=initialBalance;
+        foreach(int it in transactions)
+        {
+            if(it>=0)
+            {
+                balance+=it;
+            }
+            else
+            {
+                if (balance + it >= 0)
+                {
+                    balance+=it;
+                }
+            }
+        }
+        return balance;
+    }
+    static void Main()
+    {
+        int initialBalance=100;
+        int[] transaction={50,-30,-150,20};
+        int finalBalance=BankTransaction(initialBalance,transaction);
+        Console.WriteLine("Final Balance: "+finalBalance);
+    }
+}
