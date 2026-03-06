11811e7bfcab5cdf9a2d3d69e05cb61595e5c021
harshkr-cs <harshwwe555@gmail.com>
2026-03-06 14:47:58 +0530

    NUnit

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] items = {
+            "1:Harsh",
+            "2:Aman",
+            "1:Ravi",
+            "3:Neha",
+            "2:Simran"
+        };
+
+        string[] distinctNames = items.DistinctById();
+
+        Console.WriteLine(string.Join(", ", distinctNames));
+    }
+}
